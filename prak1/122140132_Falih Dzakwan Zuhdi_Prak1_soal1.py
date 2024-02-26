e12addda69293d1d0d928502e0073c09004659a8
Falih <falihdzakwan028@gmail.com>
2024-02-26 07:25:24 +0700

    soal 1

--- prak1/122140132_Falih Dzakwan Zuhdi_Prak1_soal1.py
+++ prak1/122140132_Falih Dzakwan Zuhdi_Prak1_soal1.py
@@ -1,4 +1,13 @@
-n = int(input("Masukkan nilai n: "))
+batas_bawah = int(input("Masukkan batas bawah: "))
+batas_atas = int(input("Masukkan batas atas: "))
+jumlah = 0
 
-for i in range(n):
-    print("*" * n)
+if batas_atas & batas_bawah < 0:
+    print("Batas atas dan bawah tidak boleh di bawah nol!")
+else:
+    for i in range(batas_bawah, batas_atas + 1):
+        if i % 2 == 1:
+            print(i)
+            jumlah += i
+        
+    print("Jumlah =", jumlah)
